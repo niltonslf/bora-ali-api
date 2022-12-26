fd35b9a10c755737c4c84cf88e4144cd1a34234c
niltonslf <niltonrck@gmail.com>
2022-12-26 04:17:56 -0300

    fix: user creation

--- database/migrations/1570280404552_users.ts
+++ database/migrations/1570280404552_users.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.timestamp('createdAt', { useTz: true })
-      table.timestamp('updatedAt', { useTz: true })
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
       table.string('name')
       table.string('email')
       table.string('profilePicture')
