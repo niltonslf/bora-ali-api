cf18092784fad1415245ee619ef0298f225eaddc
niltonslf <niltonrck@gmail.com>
2023-02-08 18:23:54 -0300

    feat: make routes private

--- start/routes.ts
+++ start/routes.ts
@@ -4,24 +4,25 @@ Route.group(() => {
   Route.post('/auth', 'AuthController.auth')
 
   Route.post('/user', 'UsersController.create')
-  Route.get('/category', 'CategoriesController.findAll')
-  Route.post('/category', 'CategoriesController.store')
-
-  Route.get('/place-type', 'PlaceTypesController.findAll')
-  Route.post('/place-type', 'PlaceTypesController.store')
-
-  Route.get('/music-style', 'MusicStylesController.findAll')
-  Route.post('/music-style', 'MusicStylesController.store')
-
-  Route.get('/image', 'ImagesController.findAll')
-  Route.post('/image', 'ImagesController.store')
-
-  Route.get('/event', 'EventsController.findAll')
-  Route.get('/event/location', 'EventsController.findByLocation')
-  Route.get('/event/:id', 'EventsController.findById')
-  Route.post('/event', 'EventsController.store')
 
   Route.group(() => {
+    Route.get('/category', 'CategoriesController.findAll')
+    Route.post('/category', 'CategoriesController.store')
+
+    Route.get('/place-type', 'PlaceTypesController.findAll')
+    Route.post('/place-type', 'PlaceTypesController.store')
+
+    Route.get('/music-style', 'MusicStylesController.findAll')
+    Route.post('/music-style', 'MusicStylesController.store')
+
+    Route.get('/image', 'ImagesController.findAll')
+    Route.post('/image', 'ImagesController.store')
+
+    Route.get('/event', 'EventsController.findAll')
+    Route.get('/event/location', 'EventsController.findByLocation')
+    Route.get('/event/:id', 'EventsController.findById')
+    Route.post('/event', 'EventsController.store')
+
     Route.get('/user/:id', 'UsersController.getById')
   }).middleware(['firebaseAuth'])
 }).prefix('/api')
